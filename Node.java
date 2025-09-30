71e77a844607c57267f9ed383f79ec4f9506da11
Henry Pham <thanhhaipham652@gmail.com>
2025-09-30 16:56:54 -0700

    Add files via upload
    Basic exercises for linked list

---
+++ Node.java
@@ -0,0 +1,21 @@
+public class Node {
+    public int value;
+    protected Node next;
+    
+    public Node(int value) {
+    this.value = value;
+    this.next = null;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
